5a54fb5e52f431d88caef1ed2fdcbad296595a9e
ejsergi <ejsergi@cias-disctemp01.main.ad.rit.edu>
2016-04-05 12:02:38 -0400

    Prepared to run 1:5

--- PREPARESTIMULI.m
+++ PREPARESTIMULI.m
@@ -4,9 +4,55 @@ close all
 addpath('Profiles and Colors/')
 addpath('OtherFunctions/')
 
-nameExp = '001';
+for nexper=1:5
+
+nameExp = sprintf('%03d',nexper);
+
+mkdir('/Volumes/myshares/Sergis share/STIMULIS/',nameExp);
 
 info = struct([]);
+%%
+%%%%
+L_Target = 50;
+Deviation = 50;
+SL_Target = 50;
+SDeviation = 50;
+sHueRange = [0 360];
+%%%%
+MEANSTOCHOI = ([-0.495:0.005:-0.4 -0.3:0.1:0]);
+
+for hue = 10:20:350
+    HueRange = [hue-10 hue+10];
+    
+    for invi=1:3
+    
+    choi = randperm(length(MEANSTOCHOI));
+    
+    for i=1:3
+        
+        meansvalues = MEANSTOCHOI(choi((i-1)*8+1:(i*8)));
+
+[TESTMoni,means,legendIm,meansSur] = CreatePatt_MeanValue...
+    (meansvalues,L_Target,Deviation,HueRange,SL_Target,SDeviation,sHueRange);
+
+info(end+1).L_Stimuli = 'All';
+info(end).Hue_Stimuli = int2str(hue);
+info(end).meansSur = meansSur;
+info(end).means = means;
+[~,index] = sortrows([means.MEANMIXTURE].');
+info(end).index = index(end:-1:1);
+
+imwrite(TESTMoni,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/S' ...
+    sprintf('%03d',length(info)) '.png']);
+imwrite(legendIm,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/L' ...
+    sprintf('%03d',length(info)) '.png']);
+    end
+    end
+    
+    disp(['Hue angle ' int2str(hue)]);
+    
+end
+%%
 %%%%
 L_Target = 50;
 Deviation = 1;
@@ -36,10 +82,11 @@ info(end).meansSur = meansSur;
 info(end).means = means;
 [~,index] = sortrows([means.MEANMIXTURE].');
 info(end).index = index(end:-1:1);
-info(end).image = TESTMoni;
-info(end).guide = legendIm;
 
-% imwrite(TESTMoni,['IMAGES/1-Changing Mean/5-30_30_All/' int2str(hue) '_' int2str(i) '.png']);
+imwrite(TESTMoni,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/S' ...
+    sprintf('%03d',length(info)) '.png']);
+imwrite(legendIm,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/L' ...
+    sprintf('%03d',length(info)) '.png']);
     end
     end
     
@@ -47,7 +94,93 @@ info(end).guide = legendIm;
     
 end
 
-infosimple = rmfield(info,{'image','guide'});
- 
-save(['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '.mat'],'info','-v7.3');
-save(['STIMULIS/' nameExp 'Simple.mat'],'infosimple','-v7.3');
+%%
+%%%%
+L_Target = 25;
+Deviation = 1;
+SL_Target = 25;
+SDeviation = 1;
+sHueRange = [0 360];
+%%%%
+MEANSTOCHOI = ([-0.495:0.005:-0.4 -0.3:0.1:0]);
+
+for hue = 10:20:350
+    HueRange = [hue-10 hue+10];
+    
+    for invi=1:3
+    
+    choi = randperm(length(MEANSTOCHOI));
+    
+    for i=1:3
+        
+        meansvalues = MEANSTOCHOI(choi((i-1)*8+1:(i*8)));
+
+[TESTMoni,means,legendIm,meansSur] = CreatePatt_MeanValue...
+    (meansvalues,L_Target,Deviation,HueRange,SL_Target,SDeviation,sHueRange);
+
+info(end+1).L_Stimuli = '25';
+info(end).Hue_Stimuli = int2str(hue);
+info(end).meansSur = meansSur;
+info(end).means = means;
+[~,index] = sortrows([means.MEANMIXTURE].');
+info(end).index = index(end:-1:1);
+
+imwrite(TESTMoni,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/S' ...
+    sprintf('%03d',length(info)) '.png']);
+imwrite(legendIm,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/L' ...
+    sprintf('%03d',length(info)) '.png']);
+    end
+    end
+    
+    disp(['Hue angle ' int2str(hue)]);
+    
+end
+
+%%
+%%%%
+L_Target = 75;
+Deviation = 1;
+SL_Target = 75;
+SDeviation = 1;
+sHueRange = [0 360];
+%%%%
+MEANSTOCHOI = ([-0.495:0.005:-0.4 -0.3:0.1:0]);
+
+for hue = 10:20:350
+    HueRange = [hue-10 hue+10];
+    
+    for invi=1:3
+    
+    choi = randperm(length(MEANSTOCHOI));
+    
+    for i=1:3
+        
+        meansvalues = MEANSTOCHOI(choi((i-1)*8+1:(i*8)));
+
+[TESTMoni,means,legendIm,meansSur] = CreatePatt_MeanValue...
+    (meansvalues,L_Target,Deviation,HueRange,SL_Target,SDeviation,sHueRange);
+
+info(end+1).L_Stimuli = '75';
+info(end).Hue_Stimuli = int2str(hue);
+info(end).meansSur = meansSur;
+info(end).means = means;
+[~,index] = sortrows([means.MEANMIXTURE].');
+info(end).index = index(end:-1:1);
+
+imwrite(TESTMoni,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/S' ...
+    sprintf('%03d',length(info)) '.png']);
+imwrite(legendIm,['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/L' ...
+    sprintf('%03d',length(info)) '.png']);
+    end
+    end
+    
+    disp(['Hue angle ' int2str(hue)]);
+    
+end
+%%
+
+save(['STIMULIS/' nameExp '.mat'],'info','-v7.3');
+
+end
+
+disp(['End exp. ' nameExp]);
