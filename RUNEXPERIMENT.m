36275d40a982b19e53017bf04a45acb0167c5d95
ejsergi <ejsergi@gmail.com>
2016-04-05 13:47:11 -0400

    RunExperiment Update

--- RUNEXPERIMENT.m
+++ RUNEXPERIMENT.m
@@ -1,15 +1,10 @@
 clear all
 close all
 
-nameExp = 'EXPERIMENT1Sergi';
+nameExp = '001';
 
-load(['/Volumes/myshares/Sergis share/ExperimentFiles/' nameExp '.mat']);
-load(['EXPERIMENTFILES/' nameExp 'Simple.mat']);
-
-
-%%
-clearvars -except info infosimple nameExp
-close all
+load(['EXPERIMENTFILES/' nameExp '.mat']);
+info = infosimple;
 
 PsychDefaultSetup(2);
 
@@ -28,24 +23,24 @@ Screen('BlendFunction', window, 'GL_SRC_ALPHA', 'GL_ONE_MINUS_SRC_ALPHA');
 permu = randperm(length(info));
 
 for i=1:length(permu);
+    
+    theImage = imread(['/Volumes/myshares/Sergis share/STIMULIS/' nameExp '/S' ...
+    sprintf('%03d',permu(i)) '.png']);
+    imageTexture = Screen('MakeTexture', window, theImage); 
+    [s1, s2, s3] = size(theImage);
+    Screen('DrawTextures', window, imageTexture);
     tic
-theImage = info(permu(i)).image;
-imageTexture = Screen('MakeTexture', window, theImage); 
-[s1, s2, s3] = size(theImage);
-Screen('DrawTextures', window, imageTexture);
-Screen('Flip', window);
 
-LastSeen = input([int2str(i) '/' int2str(length(info)) ': ']);
-info(permu(i)).LastSeen = LastSeen;
+    Screen('Flip', window);
 
-% KbWait;
+    LastSeen = input([int2str(i) '/' int2str(length(info)) ': ']);
+    info(permu(i)).LastSeen = LastSeen;
+    info(permu(i)).Time = toc;
+    
+    Screen('FillRect', window, black);
+    Screen('Flip', window);
 
-Screen('FillRect', window, black);
-Screen('Flip', window);
-
-info(permu(i)).Time = toc;
-
-WaitSecs(2);
+    WaitSecs(2);
 
 end
 
@@ -54,8 +49,5 @@ sca;
 
 info(1).Permutation = permu;
 
-
-infosimple = rmfield(info,{'image','guide'});
-save(['/Volumes/myshares/Sergis share/ExperimentFiles/' nameExp '.mat'],'info','-v7.3');
-save(['EXPERIMENTFILES/' nameExp 'Simple.mat'],'infosimple','-v7.3');
+save(['EXPERIMENTFILES/' nameExp '.mat'],'info','-v7.3');
 
